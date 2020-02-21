ee4745a7aea01f9833f3c1c5e6f68e7812d0d31e
Slav Tourachev <50384395+Tourachev@users.noreply.github.com>
2020-02-20 20:34:30 -0500

    Connected Database

--- api/app.js
+++ api/app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const apiPort = 3000;
+const db = require('./db');
+const 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,27 +19,42 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Get request for first page
+app.get('/', (req, res) => {
+	res.send('What up b!');
+});
+
+// Other routes. Managed through routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(bodyParser.json());
+
+// Db error message
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Console confirmation
+app.listen(apiPort, () => console.log(`Magic running on port ${apiPort}`));
 
 module.exports = app;
---
+++ api/models/notes-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Note = new Schema(
+	{
+		name: {type: String, required: true},
+		time: {type: [String], required: true},
+		content: {type: String, required: true}
+	},
+	{timestamps: true}
+);
+
+module.exports = mongoose.model('notes', Note);
